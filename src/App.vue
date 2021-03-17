594a27d965acfc30e275c4e3db48e936c1cb9791
Marisol Ortiz <marisolortiz_98@hotmail.com>
2021-03-17 14:40:55 -0600

    todo app base

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <div id="app"><router-view /></div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  width: 80vw;
+  @media screen and (min-width: 1024px) {
+    max-width: 400px;
+  }
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
